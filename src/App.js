7f9a10b41c9d884c5e23b49aae145a880d722f00
stepan <stepan.gurtovoy@gmail.com>
2022-04-05 18:32:42 +0300

    commit 3

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Header from './components/ui/Header'
 import Persons from './components/characters/Persons'
+import Search from './components/ui/Search'
 import './App.css'
 
 const App = () => {
 
   const [items, setItems] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [query, setQuery] = useState('')
 
   useEffect(() => {
     const fetchItems = async () => {
+      setIsLoading(true)
       const result = await axios(
-        `https://www.breakingbadapi.com/api/characters`
+        `https://www.breakingbadapi.com/api/characters?name=${query}`
       )
 
       console.log(result.data)
@@ -22,11 +25,12 @@ const App = () => {
     }
 
     fetchItems()
-  }, [])
+  }, [query])
 
   return (
     <div className='container'>
       <Header />
+      <Search getQuery={(q) => setQuery(q)}/>
       <Persons isLoading={isLoading} items={items} />
     </div>
   )
